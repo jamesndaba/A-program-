1a0368097654b461ce918fc6acd1993bce2ed614
jamesndaba <jamesndaba150@gmail.com>
2024-10-24 20:13:33 +0300

    Structures.c

---
+++ Structures.c
@@ -0,0 +1,32 @@
+//c structure 
+#include<stdio.h>
+#include<string.h>
+struct student{
+char name[30];
+float height;
+char reg_no[15];
+char email[23];
+int phone;
+}studen1,student2;
+
+
+
+
+
+
+int main(){
+//structure student.1,student.2;
+//strcpy(student1.name,"James");
+student1.heght=5.7;
+strcpy(student1.reg_no,"CT101/G/22994/24");
+strcpy(student1.email"jamesndaba150@gmail.com");
+student1.phone=0707937155;
+
+ printf("Name:%s\n"student1.name);
+printf("height:%f\n"student1.height);
+printf("regnumner:%f\n"student_reg_no);
+printf("email:%s\n"student1.email);
+printf("phone_number:%d\n",student1.phone);
+return 0;
+}
+    
